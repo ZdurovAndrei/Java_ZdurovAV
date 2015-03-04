100eda6532bc25cbb2d25486b6d82ffda5f05048
Kniaz123 <zdurovandrei@mail.ru>
2015-03-04 14:35:24 +0300

    Разбор строки

--- LogFileParser.java
+++ LogFileParser.java
@@ -1,14 +1,23 @@
 import java.io.IOException;
-import java.util.regex.*;
 import java.lang.String;
 
 public class LogFileParser {
     public static void parseFile(String line) throws IOException {
-        String Alphabet = "[A-Za-z0-9]";
-        Pattern pattern = Pattern.compile(Alphabet + " - - [" + Alphabet + "] U0022" + Alphabet + "U0022" + Alphabet);
+        /*String Alphabet = "[A-Za-z0-9\\s:.-/]";
+        Pattern pattern = Pattern.compile(Alphabet + " - - [" + Alphabet + "] \"" + Alphabet + "\" " + Alphabet);
 
-        String[] splitFile = pattern.split (line);
+        String[] splitFile = pattern.split (line);*/
 
-        System.out.println(splitFile[0]);
+        String[] buferLine = line.split(" - - ");
+        String host = buferLine[0];
+        String[] buferLine1 = buferLine[1].split(" \"");
+        String timeStamp = buferLine1[0];
+        buferLine = buferLine1[1].split("\" ");
+        String request = buferLine[0];
+        buferLine1 = buferLine[1].split(" ");
+        String replyCode = buferLine1[0];
+        String bytes = buferLine1[1];
+
+        System.out.println(" Host: " + host + "  TimeStamp: " + timeStamp  + "  Request: " + request + "  ReplyCode: " + replyCode + "  Bytes: " + bytes);
     }
 }
